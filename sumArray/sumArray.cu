5b5d3aca600496757d51fc2ca74ec9160893fbec
Hisashi Itoh <it.hisas@gmail.com>
2018-03-20 14:01:47 +0000

    sumArrayの実装中

--- sumArray/sumArray.cu
+++ sumArray/sumArray.cu
@@ -9,6 +9,19 @@ void sumArrayOnHost(float *A, float *B, float *C, cont in N){
 }
 
 //データ初期化
-void initialData(){
+void initialData(float *ip, int size){
+  // 乱数シードの作成
+  time_t t;
+  srand((unsigned int)time(&t));
+  for(int i = 0; i < size; i++){
+    ip[i] = (float)(rand() & 0xFF) / 10.0f;
+  }
+  return;
+}
+
+int main(int argc, char** argv){
+  int nElem = 1024;
+
+  //  工事中...
   
 }
