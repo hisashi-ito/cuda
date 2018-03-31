e3b8561a0089fde954de371d1a1a6c16859d5f8c
Hisashi Itoh <it.hisas@gmail.com>
2018-03-31 16:55:53 +0900

    thrustのサンプルコードを追加

---
+++ thrust/pw_multiplies.cu
@@ -0,0 +1,42 @@
+//
+// 【pw_multiplies】
+//
+//  概要: thrust のサンプルコード
+//        vector の同じ要素同士の掛け算を計算する
+//        pointwise multiplication 計算
+//
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/copy.h>
+#include <thrust/transform.h>
+#include <iostream>
+
+int main(){
+  // C++11 からこの方法で初期化可能
+  // データ列を指定して動的配列を生成できる
+  // STL vector と同じ
+  // host 側のメモリを確保 
+  thrust::host_vector<int> h_A{1,2,3};
+  thrust::host_vector<int> h_B{4,5,6};
+  thrust::host_vector<int> h_C(3);
+  
+  // device 側のメモリを確保
+  thrust::device_vector<int> d_A(3);
+  thrust::device_vector<int> d_B(3);
+  thrust::device_vector<int> d_C(3);
+  
+  // GPU(device)側へデータを転送
+  thrust::copy(h_A.begin(), h_A.end(), d_A.begin());
+  thrust::copy(h_B.begin(), h_B.end(), d_B.begin());
+  
+  // pointwise multiplicationの計算
+  // カーネル関数を直接記載しなくても計算できる(ゆとり...)
+  thrust::transform(d_A.begin(), d_A.end(), d_B.begin(), d_C.begin(), thrust::multiplies<int>());
+  
+  // device側からhost側へ転送
+  thrust::copy(d_C.begin(), d_C.end(), h_C.begin());
+  
+  // 確認
+  std::cout << h_C[0] << ", " << h_C[1] << ", " << h_C[2] << std::endl;
+  return(0);
+}
