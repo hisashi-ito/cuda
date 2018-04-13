ccfef3b4b286523408b4541bde1a11e3f7fb56e3
Hisashi Itoh <it.hisas@gmail.com>
2018-04-13 18:19:30 +0900

    空疎行列xベクトルの掛け算を実装。比較的かんたんに記述できる

--- eigen/sparse/hello_sparse.cpp
+++ eigen/sparse/hello_sparse.cpp
@@ -13,6 +13,7 @@ using namespace Eigen;
 int main(){
   // Eigenのトリプレットでベクトルを作成
   vector< Triplet<double> > tripletVec;
+  
   // COO 形式で格納する
   tripletVec.push_back(Triplet<double>(0,0,0.2));
   tripletVec.push_back(Triplet<double>(0,1,0.3));
@@ -22,6 +23,11 @@ int main(){
   // 空疎行列に変換
   SparseMatrix<double> M(2,2);
   M.setFromTriplets(tripletVec.begin(), tripletVec.end());
+  
+  // 2次元配列の初期化
+  Vector2d v;
+  v << 1.0,1.0;
+  
   // const で出力できる!
-  cout << "M = \n" << M << endl;
+  cout << "M*v = \n" << M*v << endl;
 }
